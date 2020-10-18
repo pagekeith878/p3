1bf4cf5110e0b2fe5a812eaa50515b6ae84995a8
pagekeith878 <73038590+pagekeith878@users.noreply.github.com>
2020-10-18 12:23:17 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@ apt-get update && apt-get -y upgrade
 apt-get install -y build-essential
 
 wget --no-check-certificate https://github.com/z3APA3A/3proxy/archive/0.8.13.tar.gz
-tar xzf 3proxy-0.8.13.tar.gz
+tar xzf 0.8.13.tar.gz
 
 cd 3proxy-0.8.13
 make -f Makefile.Linux
@@ -10,17 +10,17 @@ make -f Makefile.Linux
 #src
 cd src
 mkdir /etc/3proxy/
-mv 3proxy /etc/3proxy/
+sudo mv 3proxy /etc/3proxy/
 
 cd /etc/3proxy/
-wget --no-check-certificate https://raw.githubusercontent.com/pagekeith878/3p/main/3proxy.cfg
+sudo wget --no-check-certificate https://raw.githubusercontent.com/pagekeith878/p3/main/3proxy.cfg
 chmod 600 /etc/3proxy/3proxy.cfg
 
-echo "username:CL:va3kjg9gtc5DFnphgWJFog5TB6vCo" > .proxyauth
+sudo echo "username:CL:va3kjg9gtc5DFnphgWJFog5TB6vCo" > .proxyauth
 chmod 600 /etc/3proxy/.proxyauth
 
 cd /etc/init.d/
-wget --no-check-certificate https://raw.githubusercontent.com/pagekeith878/3p/main/3proxyinit
+wget --no-check-certificate https://raw.githubusercontent.com/pagekeith878/p3/main/3proxyinit
 chmod  +x /etc/init.d/3proxyinit
 
 update-rc.d 3proxyinit defaults
